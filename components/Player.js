dcac4b3e606146acb9318f798559c87ca485812b
克喵爱吃卤面 <mcysw0815@gmail.com>
2025-08-11 20:46:52 +0800

    Update Player.js

--- components/Player.js
+++ components/Player.js
@@ -73,7 +73,7 @@ const Player = () => {
           preload='auto'
           api={siteConfig(
             'MUSIC_PLAYER_METING_API',
-            'https://meting.051531.xyz/meting'
+            'https://meting.051531.xyz'
           )}
           autoplay={autoPlay}
           order={siteConfig('MUSIC_PLAYER_ORDER')}

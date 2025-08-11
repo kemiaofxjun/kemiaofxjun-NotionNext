866511bb81bde3bb357f0004132ef4c386f7a27a
克喵爱吃卤面 <mcysw0815@gmail.com>
2025-08-11 16:22:03 +0800

    Update contact.config.js

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -1,7 +1,7 @@
 /**
  * 社交按钮相关的配置同意放这
  */
-module.exports = {
+module。exports = {
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL:
     (process.env.NEXT_PUBLIC_CONTACT_EMAIL &&
@@ -19,5 +19,6 @@ module.exports = {
   CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || '', // Youtube主页
   CONTACT_XIAOHONGSHU: process.env.NEXT_PUBLIC_CONTACT_XIAOHONGSHU || '', // 小红书主页
   CONTACT_ZHISHIXINGQIU: process.env.NEXT_PUBLIC_CONTACT_ZHISHIXINGQIU || '', // 知识星球
+  CONTACT_NETEASE: process.env.NEXT_PUBLIC_CONTACT_NETEASE || '',//网易云音乐  
   CONTACT_WEHCHAT_PUBLIC: process.env.NEXT_PUBLIC_CONTACT_WEHCHAT_PUBLIC || '' // 微信公众号 格式：https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=【xxxxxx】==#wechat_redirect
 }
